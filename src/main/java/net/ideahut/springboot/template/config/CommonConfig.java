d137be0cc8fd3a89acc45d8092b646bbc6e67a01
Ideahut <ideahut.apps.team@gmail.com>
2025-08-24 13:16:48 +0700

    Initial

---
+++ src/main/java/net/ideahut/springboot/template/config/CommonConfig.java
@@ -0,0 +1,111 @@
+package net.ideahut.springboot.template.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.ideahut.springboot.api.ApiConfigHelper;
+import net.ideahut.springboot.entity.EntityApiExcludeParam;
+import net.ideahut.springboot.entity.EntityAuditParam;
+import net.ideahut.springboot.entity.EntityTrxManager;
+import net.ideahut.springboot.entity.EntityTrxManagerImpl;
+import net.ideahut.springboot.helper.FrameworkHelper;
+import net.ideahut.springboot.helper.ObjectHelper;
+import net.ideahut.springboot.job.JobConfigHelper;
+import net.ideahut.springboot.mapper.DataMapper;
+import net.ideahut.springboot.mapper.DataMapperImpl;
+import net.ideahut.springboot.message.entity.Language;
+import net.ideahut.springboot.message.entity.Message;
+import net.ideahut.springboot.serializer.BinarySerializer;
+import net.ideahut.springboot.serializer.DataMapperBinarySerializer;
+import net.ideahut.springboot.serializer.HessianBinarySerializer;
+import net.ideahut.springboot.serializer.JdkBinarySerializer;
+import net.ideahut.springboot.sysparam.entity.SysParam;
+import net.ideahut.springboot.template.properties.AppProperties;
+
+@Configuration
+class CommonConfig {
+
+	/*
+	 * DATA MAPPER
+	 */
+	@Bean
+	DataMapper dataMapper() {
+		DataMapper dataMapper = new DataMapperImpl();
+		FrameworkHelper.setDefaultDataMapper(dataMapper);
+		return dataMapper;
+	}
+	
+	
+	/*
+	 * BINARY SERIALIZER
+	 */
+	@Bean
+	BinarySerializer binarySerializer(
+		AppProperties appProperties,
+		DataMapper dataMapper
+	) {
+		BinarySerializer binarySerializer;
+		String code = ObjectHelper.useOrDefault(appProperties.getBinarySerializer(), "").trim().toLowerCase();
+		if ("xml".equals(code)) {
+			binarySerializer = new DataMapperBinarySerializer().setMapper(dataMapper).setFormat(DataMapper.XML);
+		}
+		else if ("jdk".equals(code)) {
+			binarySerializer = new JdkBinarySerializer();
+		}
+		else if ("hessian1".equals(code)) {
+			binarySerializer = new HessianBinarySerializer().setVersion(1);
+		}
+		else if ("hessian2".equals(code)) {
+			binarySerializer = new HessianBinarySerializer().setVersion(2);
+		}
+		else {
+			binarySerializer = new DataMapperBinarySerializer().setMapper(dataMapper).setFormat(DataMapper.JSON);
+		}
+		FrameworkHelper.setDefaultBinarySerializer(binarySerializer);
+		return binarySerializer;
+	}
+	
+	
+	/*
+	 * ENTITY TRX MANAGER
+	 */
+	@Bean
+	EntityTrxManager entityTrxManager(
+		AppProperties appProperties
+	) {
+		return new EntityTrxManagerImpl()
+		
+		// Entity / Model yang tidak memiliki anotasi @ApiExclude, dan tidak ingin dipublikasikan oleh ApiService
+		.setApiExcludeParams(
+			new EntityApiExcludeParam()
+			.addEntityClasses(ApiConfigHelper.getApiExcludeEntities())
+			.addEntityClasses(JobConfigHelper.getApiExcludeEntities())
+			.addEntityClasses(
+				SysParam.class,
+				Language.class,
+				Message.class
+			)
+		)
+		
+		// Entity / Model yang tidak memiliki anotasi @Audit, dan ingin setiap perubahannya disimpan
+		.setAuditParams(
+			new EntityAuditParam()
+			.addEntityClasses(ApiConfigHelper.getAuditEntities())
+			.addEntityClasses(JobConfigHelper.getAuditEntities())
+			.addEntityClasses(
+				SysParam.class,
+				Language.class,
+				Message.class
+			)
+		)
+		
+		// Daftar EntityPreListener & EntityPostListener, default autodetect = true
+		.setEntityListenerParam(null)
+		
+		// Parameter untuk menghandle anotasi @ForeignKeyEntity
+		// Ini solusi jika terjadi error saat membuat native image dimana entity memiliki @ManyToOne & @OneToMany
+		// tapi package-nya berbeda dengan package project (error ByteCodeProvider saat runtime)
+		.setForeignKeyParam(appProperties.getForeignKey());
+	}
+	
+}
---
+++ src/main/java/net/ideahut/springboot/template/config/RuntimeHintsConfig.java
@@ -0,0 +1,91 @@
+package net.ideahut.springboot.template.config;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+
+import lombok.extern.slf4j.Slf4j;
+import net.ideahut.springboot.api.ApiConfigHelper;
+import net.ideahut.springboot.helper.FrameworkHelper;
+import net.ideahut.springboot.helper.NativeImageHelper;
+import net.ideahut.springboot.helper.ObjectHelper;
+import net.ideahut.springboot.object.StringSet;
+import net.ideahut.springboot.serializer.DataMapperBinarySerializer;
+import net.ideahut.springboot.serializer.HessianBinarySerializer;
+import net.ideahut.springboot.serializer.JdkBinarySerializer;
+import net.ideahut.springboot.template.Application;
+
+@Slf4j
+@Configuration
+@ImportRuntimeHints({RuntimeHintsConfig.Registrar.class})
+public class RuntimeHintsConfig {
+	
+	RuntimeHintsConfig() {}
+
+	static class Registrar implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader loader) {
+			/*
+			 * Tambah class-class yang akan diregistrasi untuk native di sini
+			 */
+			
+		}
+		
+	}
+	
+	private static final File serializationFile = new File("serialization.tmp");
+	private static final File metadataFile = new File("src/main/resources/META-INF/native-image");
+	public static void registerToNativeImageAgent(ApplicationContext applicationContext) {
+		ObjectHelper.runIf(
+			!FrameworkHelper.inNativeImage().booleanValue() &&  
+			NativeImageHelper.isAotEnabled(), 
+			() -> NativeImageHelper.registerMetadata(collectClasses(applicationContext), serializationFile)
+		);
+	}
+	
+	private static List<Class<?>> collectClasses(ApplicationContext applicationContext) {
+		StringSet names = new StringSet(NativeImageHelper.Module.allModuleClassNames(applicationContext));
+		names.addAll(NativeImageHelper.Module.getJsonWebTokenClassNames());
+		names.addAll(NativeImageHelper.getAllClassNameInPackage("org.springframework.data.domain", false, true)); // untuk repo
+		names.add(Application.Package.APPLICATION + ".app.AppAdvice");
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".controller"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".interceptor"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".job"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".listener"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".object"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".properties"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".repo"));
+		names.addAll(NativeImageHelper.getAllClassNameInPackage(Application.Package.APPLICATION + ".service"));
+		
+		Set<Class<?>> classes = new LinkedHashSet<>(NativeImageHelper.convertToClasses(names, true));
+		classes.add(DataMapperBinarySerializer.class);
+		classes.add(JdkBinarySerializer.class);
+		classes.add(HessianBinarySerializer.class);
+		classes.addAll(ApiConfigHelper.getAllDefaultProcessors());
+		
+		List<Class<?>> list = new ArrayList<>(classes);
+		classes.clear();
+		return list;
+	}
+	
+	
+	public static void main(String... args) {
+		NativeImageHelper.mergeSerializationToMetadata(
+			serializationFile, 
+			metadataFile,
+			(String type) -> type.indexOf("$HibernateAccessOptimizer$") != -1,
+			(String type) -> type.indexOf("$HibernateInstantiator$") != -1
+		);
+		NativeImageHelper.beautifyMetadata(metadataFile);
+	}
+	
+}
